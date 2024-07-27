8ac403438718b94c08865a908d310c5679ac7f19
LUISEDOCCOR <luiseduardoocegueda@gmail.com>
2024-07-26 20:52:22 -0600

    fix: document login route

--- routes/auth.ts
+++ routes/auth.ts
@@ -10,6 +10,42 @@ export const createAuthRouter = ({ userModel }: CreateAuthRouterProps) => {
   const authRouter = Router()
   const authController = new AuthController({ userModel })
 
+  /**
+   * @swagger
+   * /api/v1/users/login:
+   *   post:
+   *     summary: Login
+   *     description: User login.
+   *     security:
+   *       - bearerAuth: []
+   *     requestBody:
+   *        content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               username:
+   *                 type: string
+   *               password:
+   *                 type: string
+   *     responses:
+   *       '204':
+   *         description: Successful response
+   *         content:
+   *           application/json:
+   *             schema:
+   *              properties:
+   *                  jwt:
+   *                      type: string
+   *       '400':
+   *         description: All fields are necessary
+   *       '401':
+   *         description: Incorrect password
+   *       '404':
+   *         description: User not found
+   *       '500':
+   *         description: Internal server error
+   */
   authRouter.post("/login", authController.login)
   return authRouter
 }
